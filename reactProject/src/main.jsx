973d3e7fb144a342db7b5a1c5add27b1ec7962d9
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2024-11-05 11:15:13 +0100

    week 9 (dobbelen)

--- reactProject/src/main.jsx
+++ reactProject/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import Dice from './dobbel.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Dice />
   </StrictMode>,
 )
